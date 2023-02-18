45d68cdcec8b742f1c5d8b329e703782e1a0ba78
Kamalakannan R <kamalakannan.ramesh@gmail.com>
2023-02-18 13:29:20 +0000

    New Project

---
+++ xchangepractice/src/test/java/com/hope/xchangepractice/XchangepracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hope.xchangepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XchangepracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

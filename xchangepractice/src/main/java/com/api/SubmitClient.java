d5bcb4b8ba634b8a736b7f854efa45852c0e1265
Kamalakannan R <kamalakannan.ramesh@gmail.com>
2023-02-20 19:49:13 +0000

    Updated code

---
+++ xchangepractice/src/main/java/com/api/SubmitClient.java
@@ -0,0 +1,56 @@
+package com.api;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.security.InvalidKeyException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+public class SubmitClient {
+    
+    public SubmitClient(FuturesApi methods) {
+        this.methods = methods;
+    }
+
+
+    private static final String apiPath = "https://demo-futures.kraken.com/derivatives";
+    private static final String apiPublicKey = "GQgye1pDbiUEQN6nvDliHScfzVaHz0TWiq6J7K87gUJb4qZoo8mYrEJF"; // accessible
+                                                                                                           // on your
+                                                                                                           // Account
+                                                                                                           // page under
+                                                                                                           // Settings
+                                                                                                           // -> API
+    // Keys
+    private static final String apiPrivateKey = "M7jcR99c+y5eZXRlMWf+WRP0TIwKX0EC4ItpZ0xiIGDdXcNGlEV3O8KHmNVSh/Q6hxQ1yngCp/NpesubyjE0eAF/"; // accessible
+                                                                                                                                            // on
+                                                                                                                                            // your
+                                                                                                                                            // Account
+                                                                                                                                            // page
+                                                                                                                                            // under
+                                                                                                                                            // Settings
+                                                                                                                                            // ->
+                                                                                                                                            // API
+                                                                                                                                            // Keys
+    private static final int timeout = 10;
+    private static final boolean checkCertificate = true;
+
+    static FuturesApi methods = new FuturesApi(apiPath, apiPublicKey, apiPrivateKey, timeout, checkCertificate);
+
+
+    public static void sendOrder() throws KeyManagementException, InvalidKeyException, MalformedURLException, NoSuchAlgorithmException, IOException {
+             // send limit order
+            String orderType = "mkt";
+            String symbol = "pf_bchusd";
+            String side = "buy";
+            BigDecimal size = BigDecimal.ONE;
+            Object result = methods.sendOrder(orderType, symbol, side, size);
+            System.out.println("sendOrder (limit):\n" + result);
+
+    }
+
+
+
+
+
+}
--- xchangepractice/src/main/java/ta4jexamples/bots/TradingBotOnMovingBarSeries.java
+++ xchangepractice/src/main/java/ta4jexamples/bots/TradingBotOnMovingBarSeries.java
@@ -3,7 +3,11 @@ package ta4jexamples.bots;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseBar;
@@ -19,6 +23,7 @@ import org.ta4j.core.num.Num;
 import org.ta4j.core.rules.OverIndicatorRule;
 import org.ta4j.core.rules.UnderIndicatorRule;
 
+import com.api.SubmitClient;
 import com.hope.xchangepractice.TickerLoader;
 import com.hope.xchangepractice.strategies.MovingMomentumStrategy;
 
@@ -49,52 +54,59 @@ public class TradingBotOnMovingBarSeries {
         return series;
     }
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws InterruptedException, IOException, KeyManagementException,
+            InvalidKeyException, NoSuchAlgorithmException {
 
-        System.out.println("********************** Initialization **********************");
-        // Getting the bar series
-        BarSeries series = initMovingBarSeries(20);
+        while (true) {
+            System.out.println("********************** Initialization **********************");
+            // Getting the bar series
+            BarSeries series = initMovingBarSeries(20);
 
-        // Building the trading strategy
-        Strategy strategy = MovingMomentumStrategy.buildStrategy(series);
+            // Building the trading strategy
+            Strategy strategy = MovingMomentumStrategy.buildStrategy(series);
 
-        // Initializing the trading history
-        TradingRecord tradingRecord = new BaseTradingRecord();
-        System.out.println("************************************************************");
+            // Initializing the trading history
+            TradingRecord tradingRecord = new BaseTradingRecord();
+            System.out.println("************************************************************");
 
-        /*
-         * We run the strategy for the 50 next bars.
-         */
-        for (int i = 0; i < 50; i++) {
+            /*
+             * We run the strategy for the 50 next bars.
+             */
+            for (int i = 0; i < 50; i++) {
 
-            // New bar
-            Thread.sleep(30); // I know...
-            BarSeries liveSeries = TickerLoader.liveMarketData();
-            Bar newBar = liveSeries.getFirstBar();
-            System.out.println("------------------------------------------------------\n" + "Bar " + i
-                    + " added, close price = " + newBar.getClosePrice().doubleValue());
-            series.addBar(liveSeries.getFirstBar());
+                // New bar
+                Thread.sleep(30); // I know...
+                BarSeries liveSeries = TickerLoader.liveMarketData();
+                Bar newBar = liveSeries.getFirstBar();
+                System.out.println("------------------------------------------------------\n" + "Bar " + i
+                        + " added, close price = " + newBar.getClosePrice().doubleValue());
+                series.addBar(liveSeries.getFirstBar());
 
-            int endIndex = series.getEndIndex();
-            if (strategy.shouldEnter(endIndex)) {
-                // Our strategy should enter
-                System.out.println("Strategy should ENTER on " + endIndex);
-                boolean entered = tradingRecord.enter(endIndex, newBar.getClosePrice(), DecimalNum.valueOf(10));
-                if (entered) {
-                    Trade entry = tradingRecord.getLastEntry();
-                    System.out.println(
-                            "Entered on " + entry.getIndex() + " (price=" + entry.getNetPrice().doubleValue()
-                                    + ", amount=" + entry.getAmount().doubleValue() + ")");
-                }
-            } else if (strategy.shouldExit(endIndex)) {
-                // Our strategy should exit
-                System.out.println("Strategy should EXIT on " + endIndex);
-                boolean exited = tradingRecord.exit(endIndex, newBar.getClosePrice(), DecimalNum.valueOf(10));
-                if (exited) {
-                    Trade exit = tradingRecord.getLastExit();
-                    System.out
-                            .println("Exited on " + exit.getIndex() + " (price=" + exit.getNetPrice().doubleValue()
-                                    + ", amount=" + exit.getAmount().doubleValue() + ")");
+                int endIndex = series.getEndIndex();
+                if (strategy.shouldEnter(endIndex)) {
+                    // Our strategy should enter
+                    System.out.println("Strategy should ENTER on " + endIndex);
+                    boolean entered = tradingRecord.enter(endIndex, newBar.getClosePrice(), DecimalNum.valueOf(10));
+                    if (entered) {
+                        Trade entry = tradingRecord.getLastEntry();
+                        System.out.println(
+                                "Entered on " + entry.getIndex() + " (price=" + entry.getNetPrice().doubleValue()
+                                        + ", amount=" + entry.getAmount().doubleValue() + ")");
+                        SubmitClient.sendOrder();
+
+                    }
+                } else if (strategy.shouldExit(endIndex)) {
+                    // Our strategy should exit
+                    System.out.println("Strategy should EXIT on " + endIndex);
+                    boolean exited = tradingRecord.exit(endIndex, newBar.getClosePrice(), DecimalNum.valueOf(10));
+                    if (exited) {
+                        Trade exit = tradingRecord.getLastExit();
+                        System.out
+                                .println("Exited on " + exit.getIndex() + " (price=" + exit.getNetPrice().doubleValue()
+                                        + ", amount=" + exit.getAmount().doubleValue() + ")");
+                        SubmitClient.sendOrder();
+
+                    }
                 }
             }
         }

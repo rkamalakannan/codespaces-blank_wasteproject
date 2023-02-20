4dbec6941da0767453c50e5b5a86cecffe2a1c59
Kamalakannan R <kamalakannan.ramesh@gmail.com>
2023-02-20 20:02:15 +0000

    Updated pom xml

--- xchangepractice/src/test/java/com/hope/xchangepractice/XchangepracticeApplicationTests.java
+++ xchangepractice/src/test/java/com/hope/xchangepractice/XchangepracticeApplicationTests.java
@@ -3,7 +3,7 @@ package com.hope.xchangepractice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = XchangepracticeApplicationTests.class)
 class XchangepracticeApplicationTests {
 
 	@Test

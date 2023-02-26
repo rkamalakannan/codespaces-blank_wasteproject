10d9f403c90c7b38d53ec9cacc047c8f62ab5c0e
Kamalakannan Ramesh <kamalakannan.ramesh@lowes.com>
2023-02-26 21:44:54 +0530

    Updated Dockefile

--- src/main/java/ta4jexamples/bots/TradingBotOnMovingBarSeries.java
+++ src/main/java/ta4jexamples/bots/TradingBotOnMovingBarSeries.java
@@ -1,25 +1,25 @@
 package ta4jexamples.bots;
 
-import java.time.Duration;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.Ticker;
+import org.knowm.xchange.kraken.KrakenExchange;
+import org.knowm.xchange.kraken.dto.marketdata.KrakenOHLC;
+import org.knowm.xchange.kraken.dto.marketdata.KrakenOHLCs;
+import org.knowm.xchange.kraken.service.KrakenMarketDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.ta4j.core.Bar;
-import org.ta4j.core.BarSeries;
-import org.ta4j.core.BaseBar;
-import org.ta4j.core.BaseStrategy;
-import org.ta4j.core.BaseTradingRecord;
-import org.ta4j.core.Strategy;
-import org.ta4j.core.Trade;
-import org.ta4j.core.TradingRecord;
+import org.ta4j.core.*;
 import org.ta4j.core.indicators.SMAIndicator;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
 import org.ta4j.core.num.DecimalNum;
@@ -28,7 +28,6 @@ import org.ta4j.core.rules.OverIndicatorRule;
 import org.ta4j.core.rules.UnderIndicatorRule;
 
 import com.api.SubmitClient;
-import com.hope.xchangepractice.TickerLoader;
 import com.hope.xchangepractice.strategies.MovingMomentumStrategy;
 
 /**
@@ -58,7 +57,7 @@ public class TradingBotOnMovingBarSeries {
      * @return a moving bar series
      */
     private static BarSeries initMovingBarSeries(int maxBarCount) throws IOException {
-        BarSeries series = TickerLoader.createBarSeries();
+        BarSeries series = createBarSeries();
         System.out.print("Initial bar count: " + series.getBarCount());
         // Limitating the number of bars to maxBarCount
         series.setMaximumBarCount(maxBarCount);
@@ -66,7 +65,61 @@ public class TradingBotOnMovingBarSeries {
         System.out.println(" (limited to " + maxBarCount + "), close price = " + LAST_BAR_CLOSE_PRICE);
         return series;
     }
-    
+
+    public static BarSeries createBarSeries() {
+
+        BarSeries series = new BaseBarSeriesBuilder()
+                .withMaxBarCount(Integer.MAX_VALUE).build();
+        try {
+            KrakenExchange krakenExchange = ExchangeFactory.INSTANCE.createExchange(KrakenExchange.class);
+            KrakenMarketDataService krakenMarketDataService = (KrakenMarketDataService) krakenExchange.getMarketDataService();
+            LocalDateTime time = LocalDateTime.now().minusWeeks(1);
+            ZoneId zoneId = ZoneId.systemDefault();
+            long epoch = time.atZone(zoneId).toEpochSecond();
+            KrakenOHLCs krakenOHLCs = krakenMarketDataService.getKrakenOHLC(new CurrencyPair("BTC", "USD"), 15, epoch);
+            Ticker ticker = krakenExchange.getMarketDataService().getTicker(new CurrencyPair("BTC", "USD"));
+//            .getKr(new CurrencyPair("BTC", "USD"), 15, epoch);
+            System.out.println("Inside KrakenOHLCLoader" + ticker.toString());
+            System.out.println("Inside OHLC" + ticker);
+            for (KrakenOHLC krakenOHLC : krakenOHLCs.getOHLCs()) {
+                BaseBar bar = new BaseBar(
+                        Duration.ofMinutes(15),
+                        ZonedDateTime.ofInstant(Instant.ofEpochSecond(krakenOHLC.getTime()),
+                                ZoneId.systemDefault()),
+                        krakenOHLC.getOpen(),
+                        krakenOHLC.getHigh(),
+                        krakenOHLC.getLow(),
+                        krakenOHLC.getClose(),
+                        krakenOHLC.getVolume());
+                series.addBar(bar);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return series;
+    }
+
+    public static BarSeries liveMarketData() throws IOException {
+        BarSeries liveSeries = new BaseBarSeriesBuilder().build();
+        KrakenMarketDataService marketDataService = marketData();
+        Ticker ticker = marketDataService.getTicker(CurrencyPair.BTC_USD);
+
+        BaseBar baseBar =  new BaseBar(Duration.ofSeconds(1), ZonedDateTime.now(), ticker.getOpen(), ticker.getHigh(),
+                ticker.getLow(), ticker.getLast(),
+                ticker.getVolume());
+
+        liveSeries.addBar(baseBar);
+
+        return liveSeries;
+
+
+    }
+
+    private static KrakenMarketDataService marketData() {
+        Exchange krakenExchange = ExchangeFactory.INSTANCE.createExchange(KrakenExchange.class);
+        return (KrakenMarketDataService) krakenExchange.getMarketDataService();
+    }
     
     public static void initateRun() throws InterruptedException, IOException, KeyManagementException,
     InvalidKeyException, NoSuchAlgorithmException {
@@ -91,7 +144,7 @@ public class TradingBotOnMovingBarSeries {
 
                 // New bar
                 Thread.sleep(30); // I know...
-                BarSeries liveSeries = TickerLoader.liveMarketData();
+                BarSeries liveSeries = liveMarketData();
                 Bar newBar = liveSeries.getFirstBar();
                 System.out.println("------------------------------------------------------\n" + "Bar " + i
                         + " added, close price = " + newBar.getClosePrice().doubleValue());

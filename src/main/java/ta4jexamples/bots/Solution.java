6f7502a57e884e684b8b2e43581c258db387e376
Kamalakannan Ramesh <kamalakannan.ramesh@lowes.com>
2023-02-26 21:24:16 +0530

    Updated Dockefile

--- src/main/java/ta4jexamples/bots/Solution.java
+++ src/main/java/ta4jexamples/bots/Solution.java
@@ -1,6 +1,5 @@
 package ta4jexamples.bots;
 
-import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.Ticker;
@@ -17,11 +16,14 @@ public class Solution {
   public static void main(String[] args) throws IOException {
 
     KrakenExchange krakenExchange = ExchangeFactory.INSTANCE.createExchange(KrakenExchange.class);
+    KrakenMarketDataService krakenMarketDataService = (KrakenMarketDataService) krakenExchange.getMarketDataService();
     LocalDateTime time = LocalDateTime.now().minusWeeks(1);
     ZoneId zoneId = ZoneId.systemDefault();
     long epoch = time.atZone(zoneId).toEpochSecond();
-    Ticker krakenOHLCs = krakenExchange.getMarketDataService().getTicker(new CurrencyPair("BTC", "USD"));
+    KrakenOHLCs krakenOHLCs = krakenMarketDataService.getKrakenOHLC(new CurrencyPair("BTC", "USD"), 15, epoch);
+    Ticker ticker = krakenExchange.getMarketDataService().getTicker(new CurrencyPair("BTC", "USD"));
 //            .getKr(new CurrencyPair("BTC", "USD"), 15, epoch);
-    System.out.println("Inside Solution"+krakenOHLCs.toString());
+    System.out.println("Inside Solution"+ ticker.toString());
+    System.out.println("Inside OHLC"+ ticker);
   }
 }

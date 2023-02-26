644ea25d43bd4e001a54985d7d1dfb40ba0adfec
Kamalakannan Ramesh <kamalakannan.ramesh@lowes.com>
2023-02-26 17:57:42 +0530

    Updated Dockefile

--- src/main/java/com/Solution.java
+++ src/main/java/com/Solution.java
@@ -3,6 +3,7 @@ package com;
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.kraken.KrakenExchange;
 import org.knowm.xchange.kraken.dto.marketdata.KrakenOHLCs;
 import org.knowm.xchange.kraken.service.KrakenMarketDataService;
@@ -15,11 +16,11 @@ public class Solution {
 
   public static void main(String[] args) throws IOException {
 
-    Exchange krakenExchange = ExchangeFactory.INSTANCE.createExchange(KrakenExchange.class);
-    KrakenMarketDataService marketDataService = (KrakenMarketDataService) krakenExchange;
+    KrakenExchange krakenExchange = ExchangeFactory.INSTANCE.createExchange(KrakenExchange.class);
     LocalDateTime time = LocalDateTime.now().minusWeeks(1);
     ZoneId zoneId = ZoneId.systemDefault();
     long epoch = time.atZone(zoneId).toEpochSecond();
-    KrakenOHLCs krakenOHLCs = marketDataService.getKrakenOHLC(new CurrencyPair("BTC", "USD"), 15, epoch);
+    Ticker krakenOHLCs = krakenExchange.getMarketDataService().getTicker(new CurrencyPair("BTC", "USD"));
+//            .getKr(new CurrencyPair("BTC", "USD"), 15, epoch);
   }
 }

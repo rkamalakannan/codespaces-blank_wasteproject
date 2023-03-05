37639924afeff3f6be727ddaaffb67aad197505a
Kamalakannan R <kamalakannan.ramesh@gmail.com>
2023-03-06 01:26:01 +0530

    Updated matic class

--- src/main/java/com/api/SubmitClient.java
+++ src/main/java/com/api/SubmitClient.java
@@ -9,6 +9,7 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.NotNull;
 
 public class SubmitClient {
 
@@ -32,7 +33,7 @@ public class SubmitClient {
     String symbol = "pf_bchusd";
     String side = "buy";
     BigDecimal size = BigDecimal.ONE;
-    Object result = methods.sendOrder(orderType, symbol, side, size, null);
+    Object result = methods.sendOrder(orderType, symbol, side, size, null, null);
     System.out.println("buyOrder (limit):\n" + result);
 
   }
@@ -43,7 +44,7 @@ public class SubmitClient {
     String symbol = "pf_bchusd";
     String side = "sell";
     BigDecimal size = BigDecimal.ONE;
-    Object result = methods.sendOrder(orderType, symbol, side, size, null);
+    Object result = methods.sendOrder(orderType, symbol, side, size, null, null);
     System.out.println("buyOrder (limit):\n" + result);
 
   }
@@ -55,7 +56,7 @@ public class SubmitClient {
  * @throws NoSuchAlgorithmException
  * @throws IOException
  */
-public static BigDecimal findTicker(String symbol) throws KeyManagementException, InvalidKeyException, MalformedURLException, NoSuchAlgorithmException, IOException {
+public static @NotNull BigDecimal findTicker(String symbol) throws KeyManagementException, InvalidKeyException, MalformedURLException, NoSuchAlgorithmException, IOException {
     ObjectMapper mapper = new ObjectMapper();
     String json = methods.getTickers();
     Result readValue = mapper.readValue(json, Result.class);
@@ -65,26 +66,26 @@ public static BigDecimal findTicker(String symbol) throws KeyManagementException
     return lastPricePerpetual;
   }
 
-  public static void buyLimitOrder(String symbol, BigDecimal limitPrice) throws KeyManagementException, InvalidKeyException, MalformedURLException, NoSuchAlgorithmException, IOException {
+  public static void buyLimitOrder(String symbol, BigDecimal limitPrice, boolean reduceOnly) throws KeyManagementException, InvalidKeyException, MalformedURLException, NoSuchAlgorithmException, IOException {
     // send limit order
     String side = "buy";
     String orderType = "lmt";
-    BigDecimal size = BigDecimal.valueOf(100);
+    BigDecimal size = BigDecimal.valueOf(1000);
 //    limitPrice = limitPrice.setScale(0, RoundingMode.DOWN);
     int leverage = 15;
 //    Object leverageResult = methods.sendMaxLeverage(leverage, symbol);
 //    System.out.println("Levaerage sell  (limit):\n" + leverageResult);
 
-    Object result = methods.sendOrder(orderType, symbol, side, size, limitPrice);
+    Object result = methods.sendOrder(orderType, symbol, side, size, limitPrice, reduceOnly);
     System.out.println("buyOrder (limit):\n" + result);
 
   }
 
-  public static void sellLimitOrder(String symbol, BigDecimal limitPrice) throws KeyManagementException, InvalidKeyException, MalformedURLException, NoSuchAlgorithmException, IOException {
+  public static void sellLimitOrder(String symbol, BigDecimal limitPrice, boolean reduceOnly) throws KeyManagementException, InvalidKeyException, MalformedURLException, NoSuchAlgorithmException, IOException {
     // send limit order
     String side = "sell";
     String orderType = "lmt";
-    BigDecimal size = BigDecimal.valueOf(100);
+    BigDecimal size = BigDecimal.valueOf(1000);
 //    limitPrice = limitPrice.setScale(0, RoundingMode.DOWN);
 
     int leverage = 15;
@@ -94,8 +95,8 @@ public static BigDecimal findTicker(String symbol) throws KeyManagementException
 //    System.out.println("Levaerage sell  (limit):\n" + leverageResult);
 
 
-    Object result = methods.sendOrder(orderType, symbol, side, size, limitPrice);
-    System.out.println("buyOrder (limit):\n" + result);
+    Object result = methods.sendOrder(orderType, symbol, side, size, limitPrice, reduceOnly);
+    System.out.println("sellOrder (limit):\n" + result);
 
   }
 

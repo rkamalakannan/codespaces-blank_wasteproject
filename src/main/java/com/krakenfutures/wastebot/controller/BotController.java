42dae5641b9a96d59fa17f29bd2fa4496d304004
Kamalakannan R <kamalakannan.ramesh@gmail.com>
2023-04-18 08:43:24 +0530

    Update BotController.java

--- src/main/java/com/krakenfutures/wastebot/controller/BotController.java
+++ src/main/java/com/krakenfutures/wastebot/controller/BotController.java
@@ -1,5 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSyste/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileS/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 

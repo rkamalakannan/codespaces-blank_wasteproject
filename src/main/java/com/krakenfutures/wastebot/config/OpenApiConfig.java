208b567de9d96ebd1193d444e02cb707ba80cedc
Kamalakannan R <kamalakannan.ramesh@gmail.com>
2026-03-01 10:17:12 +0100

    https checks

--- src/main/java/com/krakenfutures/wastebot/config/OpenApiConfig.java
+++ src/main/java/com/krakenfutures/wastebot/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.krakenfutures.wastebot.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,12 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server httpsServer = new Server();
+        httpsServer.setUrl("https://{host}");
+        httpsServer.setDescription("HTTPS Production Server");
+        
         return new OpenAPI()
+                .addServersItem(httpsServer)
                 .info(new Info()
                         .title("Wastebot Trading API")
                         .description("API for executing trades on Kraken Futures and Spot markets")
